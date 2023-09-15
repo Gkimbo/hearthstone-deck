8e6cc2bf2ace5b2a75db39b77391f1865981a24f
Garrett Tordo <76716199+PrinceFluffHK@users.noreply.github.com>
2023-09-15 18:10:16 -0400

    configured foundation and sass

--- client/src/components/About.tsx
+++ client/src/components/About.tsx
@@ -5,8 +5,7 @@ export interface IAboutProps {}
 const About: React.FunctionComponent<IAboutProps> = (props) => {
     return (
         <div className="callout">
-            <div></div>
-            Hello from about page
+            <div className="button">Hello from about page</div>
         </div>
     );
 };

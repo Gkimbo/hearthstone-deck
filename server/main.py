c08a2d59c392075a596ecfd9bae8605277e389a8
Gavin Kimball <you@example.com>
2023-09-15 10:45:58 -0400

    set up backend users

--- server/main.py
+++ server/main.py
@@ -1,6 +1,44 @@
 # main.py
+from typing import List
+from uuid import uuid4
 from fastapi import FastAPI
+from models import Gender, Role, User
 app = FastAPI()
+db: List[User] = [
+    User(
+        id=uuid4(),
+        first_name="John",
+        last_name="Doe",
+        gender=Gender.male,
+        roles=[Role.user],
+    ),
+    User(
+        id=uuid4(),
+        first_name="Jane",
+        last_name="Doe",
+        gender=Gender.female,
+        roles=[Role.user],
+    ),
+    User(
+        id=uuid4(),
+        first_name="James",
+        last_name="Gabriel",
+        gender=Gender.male,
+        roles=[Role.user],
+    ),
+    User(
+        id=uuid4(),
+        first_name="Eunit",
+        last_name="Eunit",
+        gender=Gender.male,
+        roles=[Role.admin, Role.user],
+    ),
+]
+
 @app.get("/")
 async def root():
-    return {"greeting":"Hello world"}
+    return {"Hello": "World",}
+
+@app.get("/api/v1/users")
+async def get_users():
+    return db

369a88c2278169dee2eb0360c68611b4899ab20f
Gavin Kimball <you@example.com>
2023-09-16 22:30:36 -0400

    change app name

--- server/env/bin/activate.fish
+++ server/env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/gavinkimball/python/heartstone-deck/env"
+set -gx VIRTUAL_ENV "/Users/gavinkimball/python/hearthstone-deck/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH

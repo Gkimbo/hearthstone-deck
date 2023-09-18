88b0c15e55b2e76ca54a02c04a285d5bebe79667
Garrett Tordo <76716199+PrinceFluffHK@users.noreply.github.com>
2023-09-18 11:05:56 -0400

    got interfaces working

--- client/src/components/Draft.tsx
+++ client/src/components/Draft.tsx
@@ -1,9 +1,50 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Classes from "./DraftSettings/Classes";
 
 export interface IDraftProps {}
 
 const Draft: React.FunctionComponent<IDraftProps> = (props) => {
+    // useEffect call for classes and sets
+    const getGameInfo = async () => {
+        try {
+            const response = await fetch("/api/v1/gameInfo");
+            if (!response.ok) {
+                const errorMessage = `${response.status} (${response.statusText})`;
+                const error = new Error(errorMessage);
+                throw error;
+            }
+            const responseBody = await response.json();
+            interface GameClass {
+                name: string;
+                id: number;
+            }
+            const gameClasses = responseBody.classes.map((gameClass: GameClass) => {
+                return {
+                    className: gameClass.name,
+                    classId: gameClass.id,
+                    classBool: false,
+                };
+            });
+            interface GameSet {
+                name: string;
+                id: number;
+            }
+            const gameSets = responseBody.sets.map((gameSet: GameSet) => {
+                return {
+                    setName: gameSet.name,
+                    setId: gameSet.id,
+                    setBool: false,
+                };
+            });
+        } catch (error: any) {
+            console.error(`getProjects error in Fetch: ${error.message}`);
+        }
+    };
+
+    useEffect(() => {
+        getGameInfo();
+    }, []);
+
     const gameClasses: string[] = [
         "Death Knight",
         "Demon Hunter",
@@ -17,7 +58,12 @@ const Draft: React.FunctionComponent<IDraftProps> = (props) => {
         "Warlock",
         "Warrior",
     ];
-    const [settings, setSettings] = useState({});
+    const [settings, setSettings] = useState({
+        classes: [],
+        sets: [],
+        cardsPerPack: 0,
+        numPacks: 0,
+    });
 
     return (
         <div className="callout">

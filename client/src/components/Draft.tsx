16b51812105fd95e64edac69fd12cb2bb135cfdc
Garrett Tordo <76716199+PrinceFluffHK@users.noreply.github.com>
2023-09-20 14:34:33 -0400

    attempt at understanding TS

--- client/src/components/Draft.tsx
+++ client/src/components/Draft.tsx
@@ -4,34 +4,63 @@ import Classes from "./DraftSettings/Classes";
 export interface IDraftProps {}
 
 const Draft: React.FunctionComponent<IDraftProps> = (props) => {
-    const[allCards, setAllCards] = useState<object>({})
-
-    const gameClasses = Object.keys(allCards);
-    console.log(allCards)
+    interface IDraftSettings {
+        gameClasses: Array<object>;
+        gameSets: Array<object>;
+        numPacks: number;
+    }
+    const [draftSettings, setDraftSettings] = useState<IDraftSettings>({
+        gameClasses: [],
+        gameSets: [],
+        numPacks: 0,
+    });
+    // console.log(draftSettings);
 
     const getGameInfo = async () => {
         try {
-            const response = await fetch("http://localhost:8000/api/v1/all");
+            const response = await fetch("http://localhost:8000/api/v1/info");
             if (!response.ok) {
                 const errorMessage = `${response.status} (${response.statusText})`;
                 const error = new Error(errorMessage);
                 throw error;
             }
             const responseBody = await response.json();
-            setAllCards(responseBody)
+            // console.log(responseBody);
+            interface GameData {
+                name: string;
+            }
+            const gameClasses = responseBody.classes.map((gameClass: GameData) => {
+                return {
+                    className: gameClass,
+                    classBool: false,
+                };
+            });
+
+            const gameSets = responseBody.sets.map((gameSet: GameData) => {
+                return {
+                    setName: gameSet,
+                    setBool: false,
+                };
+            });
+            const newSettings = {
+                gameClasses,
+                gameSets,
+                numPacks: 0,
+            };
+            setDraftSettings(newSettings);
         } catch (error: any) {
             console.error(`getProjects error in Fetch: ${error.message}`);
         }
     };
 
     useEffect(() => {
-        getGameInfo()
+        getGameInfo();
     }, []);
 
     return (
         <div className="callout">
             <form>
-                <Classes gameClasses={gameClasses} />
+                <Classes draftSettings={draftSettings} setDraftSettings={setDraftSettings} />
             </form>
         </div>
     );
--- client/src/components/DraftSettings/Classes.tsx
+++ client/src/components/DraftSettings/Classes.tsx
@@ -1,25 +1,47 @@
 import React from "react";
 
 export interface IClassesProps {
-    gameClasses: Array<string>;
+    draftSettings: {
+        gameClasses: Array<object>;
+        gameSets: Array<object>;
+        numPacks: number;
+    };
+    setDraftSettings: (object: {
+        gameClasses: Array<object>;
+        gameSets: Array<object>;
+        numPacks: number;
+    }) => void;
 }
 
 const Classes: React.FunctionComponent<IClassesProps> = (props) => {
-    const { gameClasses } = props;
+    const { draftSettings, setDraftSettings } = props;
+    console.log(props);
 
-    const handleChange = () => {};
+    const classElements = draftSettings.gameClasses.map((gameClass: any) => {
+        const handleCheckbox = () => {
+            type Setting = {
+                classBool: boolean
+                className: string
+            }
+            // const singleSetting = draftSettings.gameClasses.filter((singleGameClass: any) => {
+            const singleSettingArray = draftSettings.gameClasses.filter((singleGameClass: any) => {
+                return singleGameClass.className === gameClass.className;
+            });
+            const singleSetting = singleSettingArray[0];
 
-    const classElements = gameClasses.map((gameClass) => {
+            if (singleSetting.classBool)
+        };
+
         return (
-            <label htmlFor={gameClass} key={gameClass}>
+            <label htmlFor={gameClass.className}>
                 <h3>
-                    {gameClass}
+                    {gameClass.className}
                     <input
-                        id={gameClass}
+                        id={gameClass.className}
                         type="checkbox"
-                        name={gameClass}
-                        // onChange={handleChange}
-                        // value={projectRecord.projectName}
+                        name={gameClass.className}
+                        onChange={handleCheckbox}
+                        value={gameClass.classBool}
                     />
                 </h3>
             </label>
